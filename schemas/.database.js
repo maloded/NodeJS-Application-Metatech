1675bcee1c83efec70929d859a20687b172cfac6
maloded <damatomxxx@gmail.com>
2023-04-04 21:49:04 +0300

    Move codebase from DDD repo of HowProgrammingWorks

---
+++ schemas/.database.js
@@ -0,0 +1,9 @@
+({
+    name: 'messenger',
+    description: 'Messenger database',
+    version: 1,
+    driver: 'pg',
+    author: [
+        { name: 'maloded', email: 'damatomxxx@gmail.com' },
+    ],
+});
